2e33611b04c1ffe472445e9c73997076fe24a84c
Caio <caiodepaullapd@gmail.com>
2024-05-10 10:46:12 -0300

    alterações realizadas

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ const exit = document.querySelector(".exit")
 const elModal = document.querySelector(".modal")
 const btnSim= document.querySelectorAll(".botaoResposta")[0]
 const btnNao = document.querySelectorAll(".botaoResposta")[1]
+const botaoAbrir = document.querySelector(".botao_abrir")
+const botaoparagrafo = document.querySelector(".desc")
+// const logar = document
+
 
 let esconder = false
+let esconderdescricao = false
 
-exit.addEventListener("click", sairdapagina)
 
 btnSim.addEventListener("click", resposta, 'true')
 btnNao.addEventListener("click", resposta, 'false')
+exit.addEventListener("click", sairdapagina)
 
 
 function sairdapagina(){
@@ -30,5 +35,27 @@ function resposta(e){
         esconder = true
         sairdapagina()
     }
+}
 
+
+
+
+
+botaoAbrir.addEventListener("click", abrirdesc)
+
+function abrirdesc(){
+   if (esconderdescricao){
+    esconderdescricao = false
+    botaoparagrafo.style.display = 'none'
+    botaoAbrir.style.background= 'url(/assets/fechado.png)'
+    botaoparagrafo.style.transition = '2s'
+    // botaoAbrir.style.backgroundRepeat = 'no-repeat'
+
+}else{
+    esconderdescricao = true
+    botaoparagrafo.style.display = 'flex'
+    botaoAbrir.style.background= 'url(/assets/aberto.png)'
+    botaoparagrafo.style.transition = '2s'
+    // botaoAbrir.style.backgroundRepeat = 'no-repeat'
+    }
 }
