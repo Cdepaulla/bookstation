7cad429d4f1500a6d868ab57f848667697589b7c
Caio <caiodepaulladp@gmail.com>
2024-05-11 00:22:43 -0300

    restaurar

---
+++ main.js
@@ -0,0 +1,34 @@
+const exit = document.querySelector(".exit")
+const elModal = document.querySelector(".modal")
+const btnSim= document.querySelectorAll(".botaoResposta")[0]
+const btnNao = document.querySelectorAll(".botaoResposta")[1]
+
+let esconder = false
+
+exit.addEventListener("click", sairdapagina)
+
+btnSim.addEventListener("click", resposta, 'true')
+btnNao.addEventListener("click", resposta, 'false')
+
+
+function sairdapagina(){
+    if(esconder){
+        esconder = false
+        elModal.style.display = 'none'
+    }else{
+        esconder = true
+        elModal.style.display = 'flex'
+    }
+}
+
+function resposta(e){
+    if(e.target.innerHTML == 'Sim'){
+        sairdapagina()
+        window.location.href = './index.html'
+
+    }else {
+        esconder = true
+        sairdapagina()
+    }
+
+}
